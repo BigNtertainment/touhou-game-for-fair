01cd57807f417baa455529cb0fee43e72a24dc01
Pietrek14 <dawidekpe@gmail.com>
2022-04-01 20:20:46 +0200

    almost works (renderer doesnt work)

--- Makefile
+++ Makefile
@@ -9,6 +9,7 @@ EXTDIR := $(SRCDIR)/external
 
 SOURCES := $(wildcard $(SRCDIR)/*.cpp)
 GLAD := $(SRCDIR)/external/glad/glad.c
+STBI := $(SRCDIR)/external/STBI/stb.cpp
 OBJECTS := $(patsubst $(SRCDIR)/%.cpp,$(OBJDIR)/%.o,$(SOURCES))
 BUILD := $(BLDDIR)/$(NAME)
 DLLS := $(wildcard $(BLDDIR)/*.dll)
@@ -16,14 +17,16 @@ DLLS := $(wildcard $(BLDDIR)/*.dll)
 CFLAGS := 	-std=c++20 \
 			$(patsubst %,-I%,$(wildcard $(EXTDIR)/*/include)) \
 			-I$(EXTDIR)/bigngine/include/external/FreeType/include \
-			-Wall -v
+			-Wall
 LDFLAGS := $(patsubst %,-L%,$(wildcard $(EXTDIR)/*/lib)) $(patsubst $(BLDDIR)/%.dll,-l%,$(DLLS))
 
 output: $(OBJECTS)
-	$(CC) $(LDFLAGS) $< $(GLAD) -o $(BUILD)
+	$(CC) $(LDFLAGS) $< $(GLAD) $(STBI) -o $(BUILD)
 
 $(OBJDIR)/%.o: $(SRCDIR)/%.cpp
-	$(shell IF NOT EXIST $(OBJDIR) mkdir $(OBJDIR))
+ifeq ("$(wildcard $(OBJDIR))","") $(OBJDIR)
+	mkdir $(OBJDIR)
+endif
 	$(CC) $(CFLAGS) -c $< -o "$@"
 
 make run: $(BUILD)
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,45 @@
 #include "BigNgine.h"
 #include <windows.h>
 
+class FunnyBehaviour : public BigNgine::Behaviour
+{
+public:
+	void Start() override {
+		Logger::Log("hello");
+	}
+
+	void Update(int deltaTime) override {
+		parent->position.x += deltaTime / 1000.f;
+		Logger::Log(parent->position);
+	}
+};
+
 auto *Scene = new BigNgine::Scene(
 		[](BigNgine::Scene *Scene) -> void {
-			BigNgine::Entity* entity = new BigNgine::Entity();
-			
+			Logger::Log(1);
+
+			BigNgine::Entity* entity = new BigNgine::Entity(
+				BigNgine::Vector2(0, 0),
+				0.f,
+				BigNgine::Vector2(100, 100)
+			);
+
+			Logger::Log(2);
+
 			BigNgine::TextureRendererBehaviour* renderer = new BigNgine::TextureRendererBehaviour();
 
-			renderer->AddTexture("./assets/textures/mariss.png");
+			Logger::Log(3);
+
+			renderer->AddTexture("assets/img/mariss.png");
+
+			entity->AddBehaviour(renderer);
+			entity->AddBehaviour(new FunnyBehaviour());
+
+			Logger::Log(3);
 
 			Scene->AddEntity(entity);
+
+			Logger::Log(4);
 		},
 		[](BigNgine::Scene *Scene, int deltaTime)
 		{
